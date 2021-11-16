e2f7dfae7553499d6b550cbf54c5a5cdc6f37669
Monkeyhbd <monkeyhbd@163.com>
2021-11-16 21:26:17 +0800

    Adapt high resolution screen.
    Add module environment.
    Update snake_4k.py.

---
+++ module/graphic/environment.py
@@ -0,0 +1,29 @@
+import ctypes
+import tkinter
+
+from . import basic as GUIBasic
+
+# 告诉 Windows 使用程序自身的dpi适配
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+window = tkinter.Tk()
+window.title('贪吃蛇')
+
+width = window.winfo_screenwidth()
+height = window.winfo_screenheight()
+
+if height >= 2160:
+    W = 90
+elif height >= 1440:
+    W = 50
+elif height >= 1080:
+    W = 45
+else:
+    W = 30
+
+GUIBasic.W = W
+
+window.geometry('+20+20')
+window.geometry(str(int(42*W)) + 'x' + str(int(22*W)))
+window.current_page = None
+window.update()
--- snake_4k.py
+++ snake_4k.py
@@ -1,23 +1,11 @@
-import tkinter
-import ctypes
-
+import module.graphic.environment as GUIEnvironment
 import module.graphic.basic as GUIBasic
-GUIBasic.W = 90
 import module.graphic.page as GUIPage
 
 W = GUIBasic.W
 
 
-# 告诉操作系统使用程序自身的dpi适配
-ctypes.windll.shcore.SetProcessDpiAwareness(1)
-
-
 if __name__ == '__main__':
-    lmy = tkinter.Tk()
-    lmy.title('贪吃蛇')
-    lmy.geometry("+20+20")
-    lmy.geometry(str(int(42*W)) + 'x' + str(int(22*W)))  # 11: 21
-    lmy.current_page = None
-    lmy.update()
+    lmy = GUIEnvironment.window
     GUIPage.welcome_page(lmy)
     lmy.mainloop()
