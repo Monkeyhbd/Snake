e719a4d8cb5d1aec9d04fc4efde14b34fdcc7121
Monkeyhbd <monkeyhbd@163.com>
2021-11-16 09:38:44 +0800

    Update GUIBack.
    1. Delete the repeat code.
    2. Optimize level_back_create.md function.

--- module/graphic/back.py
+++ module/graphic/back.py
@@ -72,111 +72,8 @@ def level_back_create(master, board):
                 GUIBasic.wall_destroy(wall_dead_point, wall_list)
             return 1
 
-    def mdb():
-        wall_dead_point = []
-        wall_list = []
-        for _ in range(1):
-
-            # level 1
-            return_obj = GUIDoor.door_init(master.current_page.root)
-            door1 = return_obj['door1']
-            door2 = return_obj['door2']
-
-            GUIWidget.level_init(master.current_page.root, 1)
-            obj_list = GUIBasic.str_display(master=master.current_page.root, s='LEVEL 1',
-                                            x=int(master.current_page.root.winfo_width() * 0.4) + W * 2,
-                                            y=int(master.current_page.root.winfo_height() * 0.4) + W * 2,
-                                            w=W / 30 * 4.5, color='red')
-            time.sleep(1)
-            GUIBasic.obj_destroy(obj_list)
-
-            GUIBasic.wall_display(board, DataWall.level1, 'green', wall_dead_point, wall_list)
-
-            monkeyhbd = GUISnake.Snake(board, 1, 10, 15, 'black', 'red', int(W / 5), master.len_label2,
-                                       master.fps_label2, wall_dead_point)
-            GUIWidget.panel_init(master, monkeyhbd)
-            monkeyhbd.level = 1
-            monkeyhbd.setDaemon(True)
-            monkeyhbd.start()
-
-            GUIDoor.door_thread_create(door1, door2, monkeyhbd)
-            result = wait_len(monkeyhbd, 30)
-            if result == 0:
-                GUIWidget.exit_init(master.current_page.root, monkeyhbd, 'level')
-                break
-            else:
-                print('AC')
-            monkeyhbd.body_destroy()
-            monkeyhbd.food[3].destroy()
-            GUIBasic.wall_destroy(wall_dead_point, wall_list)
-
-            # level 2
-            return_obj = GUIDoor.door_init(master.current_page.root)
-            door1 = return_obj['door1']
-            door2 = return_obj['door2']
-
-            GUIWidget.level_init(master.current_page.root, 2)
-            obj_list = GUIBasic.str_display(master=master.current_page.root, s='LEVEL 2',
-                                            x=int(master.current_page.root.winfo_width() * 0.4) + W * 2,
-                                            y=int(master.current_page.root.winfo_height() * 0.4) + W * 2,
-                                            w=W / 30 * 4.5, color='red')
-            time.sleep(1)
-            GUIBasic.obj_destroy(obj_list)
-
-            GUIBasic.wall_display(board, DataWall.level2, 'green', wall_dead_point, wall_list)
-
-            monkeyhbd = GUISnake.Snake(board, 1, 10, 15, 'black', 'red', int(W / 5), master.len_label2,
-                                       master.fps_label2, wall_dead_point)
-            GUIWidget.panel_init(master, monkeyhbd)
-            monkeyhbd.level = 2
-            monkeyhbd.setDaemon(True)
-            monkeyhbd.start()
-
-            GUIDoor.door_thread_create(door1, door2, monkeyhbd)
-            result = wait_len(monkeyhbd, 30)
-            if result == 0:
-                GUIWidget.exit_init(master.current_page.root, monkeyhbd, 'level')
-                break
-            else:
-                print('AC')
-            monkeyhbd.body_destroy()
-            monkeyhbd.food[3].destroy()
-            GUIBasic.wall_destroy(wall_dead_point, wall_list)
-
-            # level 3
-            return_obj = GUIDoor.door_init(master.current_page.root)
-            door1 = return_obj['door1']
-            door2 = return_obj['door2']
-
-            GUIWidget.level_init(master.current_page.root, 3)
-            obj_list = GUIBasic.str_display(master=master.current_page.root, s='LEVEL 3',
-                                            x=int(master.current_page.root.winfo_width() * 0.4) + W * 2,
-                                            y=int(master.current_page.root.winfo_height() * 0.4) + W * 2,
-                                            w=W / 30 * 4.5, color='red')
-            time.sleep(1)
-            GUIBasic.obj_destroy(obj_list)
-
-            GUIBasic.wall_display(board, DataWall.level3, 'green', wall_dead_point, wall_list)
-
-            monkeyhbd = GUISnake.Snake(board, 1, 10, 15, 'black', 'red', int(W / 5), master.len_label2,
-                                       master.fps_label2, wall_dead_point)
-            GUIWidget.panel_init(master, monkeyhbd)
-            monkeyhbd.level = 3
-            monkeyhbd.setDaemon(True)
-            monkeyhbd.start()
-
-            GUIDoor.door_thread_create(door1, door2, monkeyhbd)
-            result = wait_len(monkeyhbd, 30)
-            if result == 0:
-                GUIWidget.exit_init(master.current_page.root, monkeyhbd, 'level')
-                break
-            else:
-                print('AC')
-
-            GUIWidget.exit_init(master.current_page.root, monkeyhbd, 'level_win')
-            # win
-
     def md():
+        '''
         for _ in range(1):
             # level 1
             rtn = level(1)
@@ -193,6 +90,11 @@ def level_back_create(master, board):
             if rtn == 0:
                 break
             # win
+        '''
+        for level_n in range(1, len(DataWall.level_box) + 1):
+            rtn = level(level_n)
+            if rtn == 0:
+                break
 
     level_back = threading.Thread()
     level_back.run = md
