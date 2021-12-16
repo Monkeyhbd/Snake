41c33bc12ece1578f2a9c05befdddb569538baec
Monkeyhbd <monkeyhbd@163.com>
2021-12-16 09:01:20 +0800

    Add logo_flash in GUILogo.

--- module/graphic/logo.py
+++ module/graphic/logo.py
@@ -1,12 +1,13 @@
 import tkinter
 import random
+import time
 
 from ..data import logo as DataLogo
 
 # The "Mokey hbd" logo's width is 38 and height is 6.
 
 
-def logo_display(master, x, y, colors, w):
+def logo_display(master, x, y, w, colors):
     obj_list = []
     logo_info = DataLogo.logo_data
     num_of_color = len(colors)
@@ -14,4 +15,17 @@ def logo_display(master, x, y, colors, w):
         pixel = tkinter.Label(master, bg=colors[random.randint(0, num_of_color - 1)])
         pixel.place(x=x + unit[0] * w, y=y + unit[1] * w, width=w, height=w)
         obj_list.append(pixel)
+    return obj_list
+
+
+def logo_flash(master, x, y, w, colors, idle):
+    obj_list = []
+    logo_info = DataLogo.logo_data
+    num_of_color = len(colors)
+    for unit in logo_info:
+        pixel = tkinter.Label(master, bg=colors[random.randint(0, num_of_color - 1)])
+        pixel.place(x=x + unit[0] * w, y=y + unit[1] * w, width=w, height=w)
+        obj_list.append(pixel)
+        master.update()
+        time.sleep(idle)
     return obj_list
--- test/logo_display.py
+++ test/logo_display.py
@@ -9,7 +9,13 @@ W = 20
 if __name__ == '__main__':
     lmy = tkinter.Tk()
     lmy.geometry('800x400')
+
     logo_display_canvas = tkinter.Canvas(lmy, bg='white')
     logo_display_canvas.place(x=0, y=0, width=38 * W, height=6 * W)
-    GUILogo.logo_display(logo_display_canvas, 0, 0, ParameterColor.mokey_logo_colors, W)
+    GUILogo.logo_display(logo_display_canvas, 0, 0, W, ParameterColor.mokey_logo_colors)
+
+    logo_flash_canvas = tkinter.Canvas(lmy, bg='white')
+    logo_flash_canvas.place(x=0, y=7 * W, width=38 * W, height=6 * W)
+    GUILogo.logo_flash(logo_flash_canvas, 0, 0, W, ParameterColor.mokey_logo_colors, 0.03)
+
     lmy.mainloop()
