c7580b3a5704f392a16600e3063615cd7d1c6e5a
Monkeyhbd <monkeyhbd@163.com>
2021-12-17 22:12:52 +0800

    Fix the scale of light_width in some screen resolution.

--- module/graphic/light.py
+++ module/graphic/light.py
@@ -18,7 +18,7 @@ def light_display(master, x, y, width, height, light_width_range, idle_range):  
     colors = ParameterColor.light_colors
     num_of_color = len(colors)
     while xx < width * W - W / 5:
-        light_width = W / 90 * 0.01 * random.uniform(light_width_range[0] * 100, light_width_range[1] * 100)
+        light_width = 0.01 * random.uniform(light_width_range[0] * 100, light_width_range[1] * 100)
         lb = tkinter.Label(master, bg=colors[random.randint(0, num_of_color - 1)])
         lb.place(x=x * W + xx, y=y * W + f3_line(x=xx, width=width, height=height),
                  width=W / 10 * light_width, height=W / 10 * light_width)
