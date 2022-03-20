a653696835da3fea6b0b9222cbab8c53c1048746
Monkeyhbd <monkeyhbd@163.com>
2022-03-20 13:08:36 +0800

    Remove score board from develop branch.

--- thread/single_back.py
+++ thread/single_back.py
@@ -2,7 +2,6 @@ import threading
 import time
 
 from widget.single import gameOverMessage as WidgetGameOverMessage
-from widget.single import scoreBoard as WidgetScoreBoard
 
 
 class SnakeMonitor(threading.Thread):
@@ -17,27 +16,5 @@ class SnakeMonitor(threading.Thread):
             time.sleep(1)
         print('Dead')
 
-        length = self.target.len
-
-        try:
-            f = open(file='data/scoreBoard.iopt', mode='r+')
-            history = eval(f.readlines()[0])
-        except (IndexError, FileNotFoundError):
-            f = open(file='data/scoreBoard.iopt', mode='w+')
-            history = []
-        history.append(length)
-        history.sort(reverse=True)
-        if len(history) > 10:
-            history = history[: 10]
-        f.seek(0)
-        f.truncate()
-        f.write(str(history))
-        f.close()
-
-        print(history)
-
         message = WidgetGameOverMessage.Message(self.page, self.target)
         message.display()
-
-        score_board = WidgetScoreBoard.ScoreBoard(self.page, data=history)
-        score_board.display()
