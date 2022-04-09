3bfdc8570d24692bdcc296245111312b1aaff1bb
Monkeyhbd <monkeyhbd@163.com>
2022-04-09 16:55:57 +0800

    Update WidgetBoard.
    Improve Board efficiency.

--- widget/board.py
+++ widget/board.py
@@ -1,5 +1,7 @@
 import tkinter
 
+from . import  common as WidgetCommon
+
 
 class Board(tkinter.Canvas):
     def __init__(self, master, h, w):
@@ -9,9 +11,12 @@ class Board(tkinter.Canvas):
         self.h = h
         self.W = master.W
         self.size = [w, h]
+        self['bg'] = '#BBFFAA'
 
         self.border: list = []
         self.wall: list = []
+
+        self.background = WidgetCommon.Box(self)
 
         for a in range(h):
             self.border.append([-self.master.W, a * self.master.W])  # W
@@ -19,15 +24,15 @@ class Board(tkinter.Canvas):
         for b in range(w):
             self.border.append([b * self.master.W, -self.master.W])  # N
             self.border.append([b * self.master.W, h * self.master.W])  # S
-        
-        self['bg'] = '#BBFFAA'
 
     def build(self):
         width = self.master.W
+        self.background.place(x=0, y=0, relwidth=1, relheight=1)
         for a in range(self.h):
             for b in range(self.w):
                 if a % 2 - b % 2 == 0:
-                    tkinter.Label(self, bg='#E4FFDD').place(x=b * width, y=a * width, width=width, height=width)
+                    tkinter.Label(self.background, bg='#E4FFDD').place(x=b * width, y=a * width,
+                                                                       width=width, height=width)
                     self.update()
 
     def display(self):
